bda344e4a6d7f52364bf338e71176d07b24d700b
erw-surf <palton@live.com>
2017-08-19 14:45:32 +0800

    wrong

---
+++ open_sesame_email.py
@@ -0,0 +1,26 @@
+"""
+Email Ip address
+"""
+
+import smtplib
+import commands
+from email.mime.text import MIMEText
+
+EMAIL = '****@gmail.com'
+PASS = '******Password****'
+
+from_x = EMAIL
+to_y = 'ewchi5@gmail.com'
+
+IP = commands.getoutput('hostname -I')
+msg = MIMEText('My New IP address is : '+ IP)
+msg['Subject'] = 'IP Address Update'
+msg['From'] = from_x
+msg['To'] = to_y
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.ehlo()
+server.login(EMAIL, PASS)
+server.sendmail(from_x, to_y, msg.as_string())
+server.quit()
